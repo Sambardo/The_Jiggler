b348222c7e09eb9a0286b2827931c1260f6afbc1
Kory T <korythacher@gmail.com>
2021-05-05 16:02:51 -0700

    added the script

---
+++ TheJiggler.ps1
@@ -0,0 +1,9 @@
+﻿$Jiggles = 0
+while ($True) 
+{
+  $TheJiggleBlaster = New-Object -ComObject WScript.Shell
+  $TheJiggleBlaster.SendKeys('{F15}')
+  $Jiggles++
+  Start-Sleep -Seconds 360
+  Write-host "Jiggle Count: $Jiggles" -ForegroundColor Cyan
+}
